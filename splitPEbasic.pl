3e424712164e0c2d5e2c4f730e78279366b5da6b
Mads Albertsen <MadsAlbertsen85@gmail.com>
2012-02-24 23:41:03 +1000

    basic pe split script

---
+++ splitPEbasic.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+unless (@ARGV == 1) {die "Usage: infile\n";}
+
+my $inputfile = shift;
+my $line;
+my $count = 0;
+	
+open(IN, $inputfile) or die;
+open(OUTp1, ">p1.fa") or die;
+open(OUTp2, ">p2.fa") or die;
+
+
+while (my $line = <IN>)  {	
+	$count++;
+	if ($count == 1 or $count == 2){	
+		print OUTp1 $line;
+	}
+	if ($count == 3){	
+		print OUTp2 $line;
+	}	
+	if ($count $== 4){	
+		print OUTp2 $line;
+		$count = 0;		
+	}	
+
+close IN;
+close OUTp1;
+close OUTp2;
+
+exit;
