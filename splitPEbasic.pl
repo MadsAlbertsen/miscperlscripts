a4f8bca0e21d8065a36fa2c60b3425055b4b7682
Mads Albertsen <MadsAlbertsen85@gmail.com>
2012-02-25 00:09:01 +1000

    Now its not random code anymore..

--- splitPEbasic.pl
+++ splitPEbasic.pl
@@ -6,7 +6,10 @@ unless (@ARGV == 1) {die "Usage: infile\n";}
 
 my $inputfile = shift;
 my $line;
+my $header;
+my $prevheader;
 my $count = 0;
+my $seq;
 	
 open(IN, $inputfile) or die;
 open(OUTp1, ">p1.fa") or die;
@@ -14,18 +17,30 @@ open(OUTp2, ">p2.fa") or die;
 
 
 while (my $line = <IN>)  {	
-	$count++;
-	if ($count == 1 or $count == 2){	
-		print OUTp1 $line;
+	chomp $line;
+	if ($line =~ m/>/) {
+		$header = $line;
+		if ($count == 1){
+			print OUTp1 "$prevheader\n";
+			print OUTp1 "$seq\n";			
+		}
+		if ($count == 2){
+			print OUTp2 "$prevheader\n";
+			print OUTp2 "$seq\n";			
+			$count = 0;
+		}
+		$count++;
+		$seq = "";
+		$prevheader = $header;		
 	}
-	if ($count == 3){	
-		print OUTp2 $line;
-	}	
-	if ($count == 4){	
-		print OUTp2 $line;
-		$count = 0;		
-	}	
+	else{
+		$seq = $seq.$line;
+	}
 }
+
+print OUTp2 "$prevheader\n";
+print OUTp2 "$seq\n";			
+
 close IN;
 close OUTp1;
 close OUTp2;
