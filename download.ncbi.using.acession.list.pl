a09882a7e9083066a6dc4738432282d0b5ce3ee4
MadsAlbertsen <MadsAlbertsem@gmail.com>
2014-01-13 10:06:04 +0100

    use accession numbers to download genomes

---
+++ download.ncbi.using.acession.list.pl
@@ -0,0 +1,147 @@
+#!/usr/bin/env perl
+###############################################################################
+#
+#    scriptname
+#
+#	 Short description
+#    
+#    Copyright (C) 2012 Mads Albertsen
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+#pragmas
+use strict;
+use warnings;
+
+#core Perl modules
+use Getopt::Long;
+use Bio::Perl;
+use Bio::DB::GenBank;
+
+
+#locally-written modules
+BEGIN {
+    select(STDERR);
+    $| = 1;
+    select(STDOUT);
+    $| = 1;
+}
+
+# get input params
+my $global_options = checkParams();
+
+my $inputfile;
+
+$inputfile = &overrideDefault("inputfile.txt",'inputfile');
+
+######################################################################
+# CODE HERE
+######################################################################
+
+
+open(IN, $inputfile) or die("Cannot read file: $inputfile\n");
+
+while ( my $line = <IN> ) {
+	chomp $line;   	
+	my $id = $line;
+	print "Downloading: $line\n";
+	
+	my $gb = new Bio::DB::GenBank;
+	my $seq = $gb->get_Stream_by_acc($id);
+	while( my $seq_elt =  $seq->next_seq ) {
+#	    write_sequence(">$id.gb", 'genbank', $seq_elt);
+	    write_sequence(">$id.fa", 'fasta', $seq_elt);
+	}
+}
+
+close IN;
+
+######################################################################
+# TEMPLATE SUBS
+######################################################################
+sub checkParams {
+    #-----
+    # Do any and all options checking here...
+    #
+    my @standard_options = ( "help|h+", "inputfile|i:s");
+    my %options;
+
+    # Add any other command line options, and the code to handle them
+    # 
+    GetOptions( \%options, @standard_options );
+    
+	#if no arguments supplied print the usage and exit
+    #
+    exec("pod2usage $0") if (0 == (keys (%options) ));
+
+    # If the -help option is set, print the usage and exit
+    #
+    exec("pod2usage $0") if $options{'help'};
+
+    # Compulsosy items
+    #if(!exists $options{'infile'} ) { print "**ERROR: $0 : \n"; exec("pod2usage $0"); }
+
+    return \%options;
+}
+
+sub overrideDefault
+{
+    #-----
+    # Set and override default values for parameters
+    #
+    my ($default_value, $option_name) = @_;
+    if(exists $global_options->{$option_name}) 
+    {
+        return $global_options->{$option_name};
+    }
+    return $default_value;
+}
+
+__DATA__
+
+=head1 NAME
+
+    vprobes.generateprobes.pl
+
+=head1 COPYRIGHT
+
+   copyright (C) 2012 Mads Albertsen
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=head1 DESCRIPTION
+
+
+
+=head1 SYNOPSIS
+
+script.pl  -i [-h]
+
+ [-help -h]           Displays this basic usage information
+ [-inputfile -i]      Inputfile. 
+ 
+=cut
